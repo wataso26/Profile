80368e9b18f9b029d1bcec23e3d6d7bccef2db69
lifeistech <2wata2wata2@gmail.com>
2021-05-07 18:55:05 +0900

    プロフィールアプリ応用の完成

--- app/src/main/java/app/wataso_/watanabe/profile/MainActivity.kt
+++ app/src/main/java/app/wataso_/watanabe/profile/MainActivity.kt
@@ -2,8 +2,6 @@ package app.wataso_.watanabe.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +9,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        profileImage.isVisible =false
-
-        profileCommentText.isVisible = false
 
         button1.setOnClickListener {
 
-            profileImage.isVisible = true
-            profileCommentText.isVisible = true
-            //konnni
+            profileImage.setImageResource(R.drawable.randy_image)
+
+            profileLabelText.text="名前"
+
+            profileCommentText.text="Androidメンターのランディだよ"
+
+        }
+
+        button2.setOnClickListener {
+
+            profileImage.setImageResource(R.drawable.baseball_image)
+
+            profileLabelText.text="スポーツ"
+
+            profileCommentText.text="野球観戦が好きで、スタジアムに通っている"
+
+        }
+
+        button3.setOnClickListener {
+
+            profileImage.setImageResource(R.drawable.donut_image)
+
+            profileLabelText.text="好きな食べ物"
+
+            profileCommentText.text="キャンディやドーナッツが大好き"
+
+        }
+
+        button4.setOnClickListener {
+
+            profileImage.setImageResource(R.drawable.gadget_image)
+
+            profileLabelText.text="趣味"
+
+            profileCommentText.text="ガジェットを集めて動かすこと"
+
         }
     }
 }
