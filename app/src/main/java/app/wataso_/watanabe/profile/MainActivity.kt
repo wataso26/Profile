9980db2a588ab3de0717a2d5b347d68090a1250e
lifeistech <2wata2wata2@gmail.com>
2021-05-07 17:45:48 +0900

    プロフィールアプリ完成

---
+++ app/src/main/java/app/wataso_/watanabe/profile/MainActivity.kt
@@ -0,0 +1,24 @@
+package app.wataso_.watanabe.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        profileImage.isVisible =false
+
+        profileCommentText.isVisible = false
+
+        button1.setOnClickListener {
+
+            profileImage.isVisible = true
+            profileCommentText.isVisible = true
+        }
+    }
+}
